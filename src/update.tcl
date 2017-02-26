8210758cccf2636dbcadc067ca2fcddcd854671c
Peter Kohler <kohler@sheldon.roya.ch>
2017-02-26 17:02:09 +0100

    - increase timeout further - added progress bar for downloading updates

--- src/update.tcl
+++ src/update.tcl
@@ -1,16 +1,27 @@
 
+proc updateProgress {token total current} {
+    set prg [expr $current * 100 / $total]
+    .cfg.progress configure -value $prg
+}
+
 proc updateCallsAndSummits {} {
-    
+
     global cwd updated
-    
+
     .cfg.ok configure -state disabled
     .cfg.cancel configure -state disabled
     .cfg.update configure -state disabled
     .cfg.call configure -state disabled
     .cfg.okrprt configure -state disabled
-    
+
+    ttk::progressbar .cfg.progress -mode determinate
+    label .cfg.progresslabel -font sotasmall
+    grid .cfg.progresslabel -row 6 -column 0
+    grid .cfg.progress -row 6 -column 1 -sticky ew
+
     if  {[catch {
-        set t [::http::geturl http://sota.hb9tvk.org/sotalog/summits.thm -timeout 120000]
+        .cfg.progresslabel configure -text "Updating summit list:"
+        set t [::http::geturl http://sota.hb9tvk.org/sotalog/summits.thm -timeout 600000 -progress updateProgress]
         set su [::http::data $t]
         ::http::cleanup $t
     } msg]} {
@@ -22,7 +33,8 @@ proc updateCallsAndSummits {} {
         close $fh
     }
     if  {[catch {
-        set t [::http::geturl http://sota.hb9tvk.org/sotalog/sotacalls.txt -timeout 30000]
+        .cfg.progresslabel configure -text "Updating SOTA callsigns:"
+        set t [::http::geturl http://sota.hb9tvk.org/sotalog/sotacalls.txt -timeout 120000]
         set sc [::http::data $t]
         ::http::cleanup $t
     } msg]} {
@@ -32,7 +44,12 @@ proc updateCallsAndSummits {} {
         puts $fh $sc
         close $fh
     }
-    
+
+    grid remove .cfg.progress
+    grid remove .cfg.progresslabel
+    destroy .cfg.progress
+    destroy .cfg.progresslabel
+
     tk_messageBox -icon info -message "Summits and calls updated successfully" -type ok
     set updated 1
     .cfg.ok configure -state normal
@@ -40,4 +57,6 @@ proc updateCallsAndSummits {} {
     .cfg.update configure -state normal
     .cfg.call configure -state normal
     .cfg.okrprt configure -state normal
+
+
 }
