4930184f659eed5617f649f41bb41744f6709205
Peter Kohler <kohler@sheldon.roya.ch>
2017-02-26 17:01:28 +0100

    - replaced 6m by 60m

--- src/main.tcl
+++ src/main.tcl
@@ -8,13 +8,11 @@ if {[info exists ::starkit::mode]} {
 }
 
 if {$tcl_platform(os) == "Darwin"} {
-    set cwd [file dirname [file dirname [file dirname [file dirname [file normalize $argv0]]]]]
+    set cwd [file dirname [file dirname [file dirname [file dirname [file dirname [file normalize $argv0]]]]]]
 }
 
-set bandlist [list 40m 7.0MHz 30m 10.1MHz 20m 14.0MHz 17m 18.0MHz 15m 21.0MHz \
-    12m 24.8MHz 10m 28MHz " 6m" 50MHz]
+set bandlist [list 60m 5.0MHz 40m 7.0MHz 30m 10.1MHz 20m 14.0MHz 17m 18.0MHz 15m 21.0MHz 12m 24.8MHz 10m 28MHz]
 array set w2f $bandlist
-
 
 set modes [list CW SSB]
 set s2s ""
